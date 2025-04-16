dc6958716017af1a2dc00644cfb12d173e55a99a
eyern <eyern254@gmail.com>
2025-04-16 19:41:57 +0300

    media url

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = "media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/app/data')
 
 
 # Default primary key field type
