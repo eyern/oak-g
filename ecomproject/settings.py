60ff9579158ebf880df47a8a3df0c634f247c3f3
eyern <eyern254@gmail.com>
2025-04-19 22:21:25 +0300

    debug reverse

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -30,7 +30,7 @@ load_dotenv
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['theoakgolf.com', 'https://theoakgolf.com', '127.0.0.1', 'http://127.0.0.1', 'web-production-a3ae.up.railway.app', 'https://web-production-a3ae.up.railway.app']
 
